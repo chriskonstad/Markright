b752f55cedc9858faeb07f6d415c1f3ddaf99929
Chris Konstad <chriskon149@gmail.com>
2016-03-20 19:31:48 -0700

    Added test for parse and filter_empty_text

--- tests.ml
+++ tests.ml
@@ -1,4 +1,5 @@
 #use "markright.ml"
 
-
 let test_flatten_replace = ("goodbye hello" = flatten (replace [Variable("hello"); Text(" "); Text("hello")] (fun e -> match e with x -> if x = "hello" then "goodbye" else x)))
+
+let test_filter_parse = ([Text "Hello \\{{world "; Variable "goodbye"; Text "!"] = filter_empty_text (parse "Hello \\{{world {{ goodbye }}!"))
